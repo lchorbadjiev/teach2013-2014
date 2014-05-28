810ee9041659b576c8ec24b6f6160ea641ba67d5
Lubomir Chorbadjiev <lchorbadjiev@setelis.com>
2014-05-28 14:01:58 +0300

    air resistance added...

--- 50/accel.m
+++ 50/accel.m
@@ -4,3 +4,13 @@ function [ax, ay] = accel(x, y, vx, vy)
 
   ax = 0.0;
   ay = -9.8;
+
+  v = sqrt(vx^2+vy^2);
+
+  ar_x = - 0.05 * vx * v;
+  ar_y = - 0.05 * vy * v;
+
+  ax = ax + ar_x;
+  ay = ay + ar_y;
+
+  
